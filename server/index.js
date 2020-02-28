2219bd19db127e864e0cab14fbac8883c6110640
Jikyubin <rbqls0502@gmail.com>
2020-02-28 19:42:01 +0900

    최초 접속 시 데이터 가져오기 -- 중복은 나중에 함수로 묶기

--- server/index.js
+++ server/index.js
@@ -25,6 +25,34 @@ const getHtml = async () =>{
     }
 };
 
+//서버 실행 시 데이터 크롤링
+getHtml()
+        .then(html => {
+            let ulList = []
+            const $ = cheeiro.load(html.data);
+            const $bodyList = $("div.data_table table.num tbody").children('tr')
+
+            // 0: 확진환자 1: 확진환자 격리해제 2: 사망자 3: 검사진행
+            $bodyList.each(function (i, elem) {
+                let title = $(this).children('th').text();
+                let data = $(this).children('td').text();
+
+                ulList[i] = {
+                    title: title,
+                    data: data
+                }
+            })
+            // console.log(ulList)
+            const data = ulList;
+            return data;
+        })
+        .then(res => {
+            coronaData = res;
+            log(coronaData);
+});
+
+
+// 매시간(테스트용 1분마다)마다 데이터 크롤링 후 프론트로 전송
 const j = schedule.scheduleJob('1 * * * * *',function(){
     getHtml()
         .then(html => {
@@ -55,9 +83,7 @@ const j = schedule.scheduleJob('1 * * * * *',function(){
             
             io.emit("coronaData",res)
         });
-
     });
-
 
 io.on("connection", socket => {
     console.log(socket.client.id); // Prints client socket id
