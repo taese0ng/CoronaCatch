aed387e8522ea5861bbe572cab6bed3c50ab204f
Jikyubin <rbqls0502@gmail.com>
2020-03-15 19:43:32 +0900

    네이버 맵사진 가져오는거 순서 바꼇길래 바꿧음

--- server/index.js
+++ server/index.js
@@ -110,7 +110,7 @@ const overlapCheck = function (dateKey) {
 const getImageSrc = function (html) {
   let src;
   const $ = cheeiro.load(html.data);
-  const $bodyImg = $("div.se_image div.se_sectionArea div.se_editArea div.se_viewArea").eq(4).children('a.se_mediaArea');
+  const $bodyImg = $("div.se_image div.se_sectionArea div.se_editArea div.se_viewArea").eq(5).children('a.se_mediaArea');
 
   src = $bodyImg.find(".se_mediaImage").attr('data-src');
 
